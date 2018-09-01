0fa0bc03d8168a51dd1b2ce00e9a9d33a42fe749
wwagner4 <wwagner4@gmail.com>
2018-09-01 15:14:48 +0200

    image creation on the server OK

--- server/src/main/scala/oneline/server/OnelineServlet.scala
+++ server/src/main/scala/oneline/server/OnelineServlet.scala
@@ -1,10 +1,9 @@
 package oneline.server
 
 import java.nio.file.{Files, Paths}
-import java.util.Base64
 
 import oneline.Loaneable
-import oneline.common.OnelineRequest
+import oneline.common.{OnelineRequest, OnelineResponse}
 import org.scalatra._
 import org.scalatra.util.io._
 
@@ -18,7 +17,7 @@ class OnelineServlet extends ScalatraServlet with Loaneable {
 
   get("/oneline-2018-client-fastopt.js") {
     val root = Paths.get(".")
-        .resolve("oneline-2018-client-fastopt.js")
+      .resolve("oneline-2018-client-fastopt.js")
     println(s"reading from $root")
     loan(Files.newInputStream(root))(i => Ok(readBytes(i)))
   }
@@ -29,30 +28,30 @@ class OnelineServlet extends ScalatraServlet with Loaneable {
       "charset" -> "utf-8",
     )
     println("in the server /index (1)")
-    Ok("""<!DOCTYPE html>
-      |<html>
-      |<head>
-      |    <meta charset="UTF-8">
-      |    <title>oneline</title>
-      |</head>
-      |<body>
-      |<script type="text/javascript" src="oneline-2018-client-fastopt.js"></script>
-      |</body>
-      |</html>
-    """.stripMargin, headers = _h)
+    Ok(
+      """<!DOCTYPE html>
+        |<html>
+        |<head>
+        |    <meta charset="UTF-8">
+        |    <title>oneline</title>
+        |</head>
+        |<body>
+        |<script type="text/javascript" src="oneline-2018-client-fastopt.js"></script>
+        |</body>
+        |</html>
+      """.stripMargin, headers = _h)
   }
 
   post("/trans") {
-    println("_in the server /trans")
+    println("in the server /trans")
     import upickle.default._
 
-    val data = read[OnelineRequest](request.body)
-    val img = data.img
-    println(s"data:'$data'")
-    println(s"img:'$img'")
-    //val base64Text = photoBase64.replaceFirst("^.*,", "")
-    val bytes = Base64.getDecoder.decode(img)
-    Ok()
+    val req = read[OnelineRequest](request.body)
+    println("creating online")
+    val resp: OnelineResponse = new ServerOnelineImageCreator().create(req)
+    val body: String = write(resp)
+    println("created online")
+    Ok(body)
   }
 
 }
---
+++ server/src/main/scala/oneline/server/ServerOnelineImageCreator.scala
@@ -0,0 +1,59 @@
+package oneline.server
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+import java.util.Base64
+
+import oneline._
+import oneline.common.{OnelineRequest, OnelineResponse}
+
+class ServerOnelineImageCreator extends Loaneable {
+
+  def create(request: OnelineRequest): OnelineResponse = {
+    val base64Text = request.img.replaceFirst("^.*,", "")
+    val bytes = Base64.getDecoder.decode(base64Text)
+    val in = new ByteArrayInputStream(bytes)
+    val out = new ByteArrayOutputStream()
+    val p1 = lineDrawerProperties(request)
+    val p2 = exportProperties(request)
+    loan(in){
+      _in => loan(out){
+        _out => new OnelineImageCreator().create(in = _in, out = _out, p1, p2)
+      }
+    }
+    val img = Base64.getEncoder.encode(out.toByteArray)
+    val imgType = p2.exportFormat.toLowerCase
+    val prefix = s"data:image/$imgType;base64,"
+
+    OnelineResponse(img = s"$prefix$img")
+  }
+
+  private def lineDrawerProperties(request: OnelineRequest) = {
+    new LineDrawerProperties {
+      override def lineLength: Int = request.lineLength
+
+      override def seed: Long = request.seed
+
+      override def distFactor: Double = request.distFactor
+
+      override def distTrimmer: Double = request.distTrimmer
+
+      override def brightnessFactor: Double = request.brightnessFactor
+
+      override def touchesFactor: Double = request.touchesFactor
+
+    }
+  }
+
+  private def exportProperties(request: OnelineRequest) = {
+    new ExportProperties {
+      override def exportWidth: Int = request.exportWidth
+
+      override def exportHeight: Int = request.exportHeight
+
+      override def exportFormat: String = request.exportFormat
+
+      override def exportLineWidth: Double = request.exportLineWidth
+
+    }
+  }
+}
